616d7a9a767b3d1a7860004719d08c607b31a9d6
arsh <honeykhangura@csiom.com>
2016-08-20 15:29:33 +0530

    first commit

---
+++ feedback/views.py
@@ -0,0 +1,15 @@
+from django.views.generic.edit import FormView
+from feedback.forms import FeedbackForm
+from feedback.tasks import amazon_uploader,create_thumb
+
+
+class FeedbackView(FormView):
+    template_name = 'feedback/contact.html'
+    form_class = FeedbackForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        amazon_uploader.delay()
+        create_thumb.delay()
+        return super(FeedbackView, self).form_valid(form)
+
