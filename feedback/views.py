56d92eddb94f1b5f82ebbc2859db70e0f693b274
metal-machine <arsh840@gmail.com>
2017-04-27 22:00:11 +0530

    removing un-necessary files

--- feedback/views.py
+++ feedback/views.py
@@ -1,8 +1,20 @@
 from django.views.generic.edit import FormView
-from feedback.forms import FeedbackForm
+from feedback.forms import FeedbackForm,UploadFileform
+
 #from feedback.tasks import amazon_uploader,create_thumb
 
-
+def UploadFile(request):
+    if request.method=='post':
+        form = UploadFileForm(request.POST,request.files)
+        if form.is_valid():
+            # how request.FILES does it's work?
+            
+            handle_uploaded_file(request.FILES['file']):
+            return HttpResponseRedirect('/success/url/')
+        else:
+            form = UploadFileForm()
+        return render(request,'feedback/contact.html',{'form': form})
+    
 class FeedbackView(FormView):
     template_name = 'feedback/contact.html'
     form_class = FeedbackForm
